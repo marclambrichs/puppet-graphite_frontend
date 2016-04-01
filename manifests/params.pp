1d13c5fc7a2ffa26f3ce426e20f806b3a7bf4d21
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-04-01 16:26:06 +0200

    Extended init with params for local_settings.py
    memcache_hosts should be memcached_hosts
    
    renamed variable to ls_memcached_hosts

--- manifests/params.pp
+++ manifests/params.pp
@@ -18,6 +18,20 @@ class graphite_web::params {
   $gw_pytz_version           = present
   $gw_pytz_pkg               = 'pytz'
   $gw_webapp_dir             = '/etc/graphite-web'
+  $ls_carbonlink_hosts       = []
+  $ls_carbonlink_timeout     = 1.0
+  $ls_conf_dir               = '/opt/graphite/conf'
+  $ls_content_dir            = '/usr/share/graphite/webapp/content'
+  $ls_databases              = {}
+  $ls_default_cache_duration = 60
+  $ls_graphite_root          = '/usr/share/graphite'
+  $ls_index_file             = '/var/lib/graphite-web/index'
+  $ls_log_dir                = '/opt/graphite/storage/log/webapp'
+  $ls_memcached_hosts        = ['127.0.0.1:11211']
+  $ls_secret_key             = 'changeme'
+  $ls_storage_dir            = '/opt/graphite/storage'
+  $ls_timezone               = 'Europe/Amsterdam'
+  $ls_whisper_dir            = '/opt/graphite/storage/whisper'
   $manage_packages           = true
   $memcached_enabled         = false
 }
